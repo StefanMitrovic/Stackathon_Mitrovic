320d14d2563616b9639bc1ba52c19f25fd48c6b3
Stefan <sm9dw@virginia.edu>
2022-05-17 22:08:07 -0400

    Container complete

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const buildTemplate = function(){
     for (let x = 0; x<25; x++){
         let row = document.createElement('div');
         row.id = "row"+(x+1);
+        row.className = "allrows row"+(x+1);
         
         for (let y = 0; y<25; y++){
             let cell = document.createElement('div');
             cell.id = "cell" + (x*10+(y+1));
+            cell.className = "allcells cell" + (x*10+(y+1));
             row.appendChild(cell);
         }
         container.appendChild(row);
