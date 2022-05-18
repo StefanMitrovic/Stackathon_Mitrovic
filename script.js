c5117f7de8d003dbd780b1abff263b6bd774084f
Stefan <sm9dw@virginia.edu>
2022-05-17 21:23:20 -0400

    Grid created

---
+++ script.js
@@ -0,0 +1,15 @@
+const buildTemplate = function(){
+    let container = document.getElementById('container');
+    for (let x = 0; x<25; x++){
+        let row = document.createElement('div');
+        row.id = "row"+(x+1);
+        
+        for (let y = 0; y<25; y++){
+            let cell = document.createElement('div');
+            cell.id = "cell" + (x*10+(y+1));
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
+    }
+    
+}
