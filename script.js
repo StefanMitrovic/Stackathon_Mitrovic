7087f161f7dc3fff5dcb4774ce1e3b141d771d7d
Stefan <sm9dw@virginia.edu>
2022-05-19 20:13:31 -0400

    working pathfinder

--- script.js
+++ script.js
@@ -1,56 +1,5 @@
-
 let maxRow = 10;
 let maxCol = 10;
-let board = new Array(maxRow);
-
-const point = function(){
-    this.f = 0;
-    this.g = 0;
-    this.h = 0;
-}
-
-
-// const buildTemplate = function() {
-//     for (let y = 0; y<maxCol; y++){
-//         board[y] = new Array(maxCol);
-//     }
-//     for (let y=0; y<maxCol; y++){
-//         for (let x=0; x<maxRow; x++){
-//             board[y][x] = new point();
-//         }
-//     }
-//     let start = board[0][0];
-//     let end = [maxCol-1][maxRow-1];
-//     openSet.push(start);
-//     return board;
-// }
-
-const solution = function(){
-    let grid = [];
-    let columnCount = 0;
-    let rowCount = 0;
-    let cellValue = 1;
-
-    for (let x=0; x<maxRow; x++){
-        grid[x] = new Array(maxRow).fill(0);
-    }
-    for (let x = 1; x<(maxRow*maxCol+1); x++){
-        if(document.getElementById('cell'+x).style.backgroundColor == builtWall){
-            grid[rowCount][columnCount] = -1;
-        } else {
-            grid[rowCount][columnCount] = cellValue;
-        }
-        columnCount++;
-        if (columnCount==maxRow){
-            rowCount++;
-            columnCount=0;
-        }
-        cellValue++
-    }
-    
-
-}
-
 
 const buildTemplate = function(){
     let container = document.getElementById('container');
@@ -58,23 +7,10 @@ const buildTemplate = function(){
         let row = document.createElement('div');
         row.id = "row"+(x+1);
         row.className = "allrows row"+(x+1);
-        //board[x] = new Array(maxCol);
         for (let y = 0; y<maxCol; y++){
             let cell = document.createElement('div');
             cell.id = "cell" + (x*10+(y+1));
             cell.className = "allcells cell" + (x*10+(y+1));
-            //board[x][y] = new point();
-
-            // if (cell.id == 'cell1'){
-            //     cell.name = 'Start';
-            //     cell.style.backgroundColor = ('green');
-            //     openSet.push(cell);
-            // }
-
-            // if (cell.id == 'cell' + (maxCol*maxRow)){
-            //     cell.name = 'End';
-            //     cell.style.backgroundColor = ('black');
-            // }
 
             if (((x*10)+(y+1)) !=(maxCol*maxRow)  && ((x*10)+(y+1)) != 0){
                 //add default color
@@ -87,11 +23,10 @@ const buildTemplate = function(){
         }
         container.appendChild(row);
     }
-    render();
 }
 let base = 'cornsilk';
 let builtWall = 'red';
-
+let path = 'green';
 
 const wall = function(elemID){
     let cell = document.getElementById(elemID);
@@ -103,18 +38,108 @@ const wall = function(elemID){
     }
 }
 
+const solution = function(){
+    let grid = [];
+    let columnCount = 0;
+    let rowCount = 0;
+    let cellValue = 1;
+    let adjacentSet = {};
+    let possibleMoveSet = [
+        [-1, 0],
+        [1, 0],
+        [0, 1],
+        [0, -1]
+    ]
+    let checkedCells = [];
+    let traversed = new Array(maxRow*maxCol).fill(0);
 
-let openSet = [];
-let closedSet = [];
-
-console.log(openSet)
-
-
-const traverse = () => {
-    if (openSet.length>0){
-        //
+    for (let x=0; x<maxRow; x++){
+        grid[x] = new Array(maxRow).fill(0);
     }
-    else{
-        console.log('No solution!')
+
+    for (let x = 1; x<(maxRow*maxCol+1); x++){
+        if(document.getElementById('cell'+x).style.backgroundColor == 'red'){
+            grid[rowCount][columnCount] = -1;
+        } else {
+            grid[rowCount][columnCount] = cellValue;
+        }
+        columnCount++;
+        if (columnCount==maxRow){
+            rowCount++;
+            columnCount=0;
+        }
+        cellValue++
     }
+
+    for (let x = 0; x<maxRow; x++){
+        for (let y = 0; y<maxCol; y++){
+            if (grid[x][y] == -1){
+                continue
+            }
+            let currentCell = grid[x][y];
+            let openSpots = [];
+
+            for (let i = 0; i<possibleMoveSet.length; i++){
+                let newRow = possibleMoveSet[i][0]+x;
+                let newCol = possibleMoveSet[i][1]+y;
+
+                if ((newCol >= 0 && newCol < maxCol) && (newRow >= 0 && newRow < maxRow)){
+                    if (grid[newRow][newCol] != -1){
+                        openSpots.push([newRow, newCol]);
+
+                    }
+                }
+            }
+            adjacentSet[currentCell] = openSpots;
+        }
+    }
+
+    for (let x = 0; x <maxRow; x++){
+        checkedCells[x] = new Array(maxRow).fill(false);
+    }
+
+    //Tracking section
+    
+    let queue = [];
+    let solved = false;
+    queue.push([0,0]);
+    while(queue.length > 0){
+        let coordinate = queue.splice(0,1)[0];
+        let cell = grid[coordinate[0]][coordinate[1]];
+        checkedCells[coordinate[0]][coordinate[1]] = true;
+
+        if (cell == 100){
+            solved = true;
+            break;
+        }
+
+        let neighbor = adjacentSet[cell];
+        for (let x = 0; x<neighbor.length; x++){
+            let check = neighbor[x];
+            if(!checkedCells[check[0]][check[1]]){
+                checkedCells[check[0]][check[1]] = true;
+                queue.push(check);
+                traversed[(grid[check[0]][check[1]])-1] = cell-1;
+            }
+        }
+    }
+    if(!solved) {
+        console.log('this maze has no solution');
+    }
+    let endPoint = grid[9][9];//change later to be more fluid
+    let startPoint = grid[0][0];
+    document.getElementById('cell'+endPoint).style.backgroundColor = path;
+    let previous = endPoint-1;
+    
+    while(true){
+        
+        let node = traversed[previous];
+        document.getElementById('cell'+(node+1)).style.backgroundColor = path;
+        if (node == 0){
+            break
+        } else {
+            previous = node;
+        }
+    }
+    document.getElementById('cell'+startPoint).style.backgroundColor = path;
 }
